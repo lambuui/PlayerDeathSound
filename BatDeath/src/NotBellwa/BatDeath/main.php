b16a1c14e62b235e9de6f9ab2a5b5f65c8fc6425
NotBella <bellakittyylover@gmail.com>
2020-07-15 23:06:27 -0400

    Add files via upload

---
+++ BatDeath/src/NotBellwa/BatDeath/main.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+namespace NotBellwa\BatDeath;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\event\player\PlayerDeathEvent;
+
+class main extends PluginBase implements Listener{
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDeath(PlayerDeathEvent $event){
+        $player = $event->getPlayer();
+        $player->getLevel()->addSound(new EndermanTeleportSound($player));
+    }
+}
